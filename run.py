932754ffc7c294440079730bae3ef7cfe4b9c492
jordyc2163 <jordycaceres00@gmail.com>
2022-10-05 23:18:48 -0400

    - User dup email, req txt, + checkpw, repr methods

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
 # optional file for the creation of shell context processors
+from app import app
+from app.models import db, User, Company, Job
+
+
+@app.shell_context_processor
+def db_context():
+    return {'db': db, 'User': User, 'Company': Company, 'Job': Job}
